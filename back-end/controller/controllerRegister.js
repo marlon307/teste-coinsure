2b5b26e4d4f9e29f0252cf92bddd81c10aec7a55
marlon307 <marlon-ramosb@hotmail.com>
2022-01-13 12:59:26 -0300

    Registe check email

--- back-end/controller/controllerRegister.js
+++ back-end/controller/controllerRegister.js
@@ -1,7 +1,14 @@
 const { checkname, checkpsw, checkemail } = require('../middleware');
+const serviceCkeckEmail = require('../service/serviceCkeckEmail')
 
-function controllerRegister(req, res, next) {
+async function controllerRegister(req, res, next) {
   const { name, email, password } = req.body;
+
+  const resultEamil = await serviceCkeckEmail(email);
+
+  if (resultEamil) {
+    return res.status(404).json(resultEamil);
+  }
 
   if (!checkname(name) || !checkpsw(password) || !checkemail(email)) {
     return res.status(404).json({
